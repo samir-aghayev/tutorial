69d1da2e80864852fe17d06cc0deb1f4101e526e
Ağayev Samir <agayev.samir8@gmail.com>
2023-12-30 12:26:17 +0400

    project created

---
+++ src/test/java/az/atlacademy/tutorials/TutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atlacademy.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
